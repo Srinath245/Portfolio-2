e71fa3d075c6708eaaf6b811f932c74193ea3fd9
Srinath <136671857+Srinath245@users.noreply.github.com>
2024-10-16 22:37:55 +0530

    Add files via upload

---
+++ profial.js
@@ -0,0 +1,34 @@
+// var log_in_box = document.getElementById("log-in");
+// var well = document.getElementById("well");
+// var first_box = document.getElementById("first-box");
+// var regiset_box = document.getElementById("register-box");
+
+// function sign_in()
+// {
+//     first_box.style.display="block";
+//     regiset_box.style.display="block";
+//     log_in_box.style.display = "block";
+
+// }
+
+var btn        = document.getElementById("btn");
+var soft_skills= document.getElementById("soft-skills");
+var btn1      = document.getElementById("btn-1");
+
+
+function ccc()
+{
+     soft_skills.style.display    ="block";
+     soft_skills.style.animationTimingFunction = 4;
+     btn1.style.display = "block";
+}
+
+function close()
+{
+     btn1.style.display="none";
+     soft_skills.style.display="none";
+}
+
+
+
+
